384d99817e465276559ab6305030674f76641bfb
Lorenz Nimmervoll <40146720150346@litec.ac.at>
2019-03-25 10:38:52 +0100

    .gitignore fix

--- Secrets.h
+++
@@ -1,8 +0,0 @@
-// create a Secrets.h file with the following:
-
-// AP mode password
-const char WiFiAPPSK[] = "your-password";
-
-// Wi-Fi network to connect to (if not in AP mode)
-char* ssid = "WLAN23";
-char* password = "57erChevy";
